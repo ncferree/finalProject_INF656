59419de121e74705b4ea8150c3c3bb1d4a0cec10
ncferree <ncferree@gmail.com>
2021-12-11 15:52:34 -0700

    Delete src directory

--- src/app/models/painting.ts
+++
@@ -1,9 +0,0 @@
-export interface Painting {
-    wall_width: number;
-    wall_height: number;
-    ceiling_width: number;
-    ceiling_length: number;
-    total_square_feet: number;
-    total_linear_feet:number;
-    condition: any;
-}
